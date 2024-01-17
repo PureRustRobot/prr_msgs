3bb3c4c839a6e18ff03b03c584a3be4d9da2c214
motii <108280115+motii8128@users.noreply.github.com>
2024-01-17 11:04:09 +0900

    Add files via upload

--- src/msg.rs
+++ src/msg.rs
@@ -77,3 +77,40 @@ pub fn deserialize_angular(str_value:String)->Angular
      let result:Angular = serde_json::from_str(&str_value).unwrap();
     result
 }
+#[derive(Serialize, Deserialize)]
+pub struct SingleMotor{
+    pub  power:f32,
+}
+pub fn serialize_singlemotor(value:&SingleMotor)->String
+{
+    serde_json::to_string(value).unwrap()
+}
+pub fn deserialize_singlemotor(str_value:String)->SingleMotor
+{
+     let result:SingleMotor = serde_json::from_str(&str_value).unwrap();
+    result
+}
+#[derive(Serialize, Deserialize)]
+pub struct Buttons{
+    pub  circle:f32,
+    pub  cross:f32,
+    pub  cube:f32,
+    pub  triangle:f32,
+    pub  up_key:f32,
+    pub  down_key:f32,
+    pub  right_key:f32,
+    pub  left_key:f32,
+    pub  r1:f32,
+    pub  r2:f32,
+    pub  l1:f32,
+    pub  l2:f32,
+}
+pub fn serialize_buttons(value:&Buttons)->String
+{
+    serde_json::to_string(value).unwrap()
+}
+pub fn deserialize_buttons(str_value:String)->Buttons
+{
+     let result:Buttons = serde_json::from_str(&str_value).unwrap();
+    result
+}
